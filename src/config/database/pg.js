00dfe0f6dda5b8eba0c20839e48650dc8c2698be
Thales Alves <thalesalves758@gmail.com>
2022-08-03 09:50:34 -0300

    fix: fix Pool import

--- src/config/database/pg.js
+++ src/config/database/pg.js
@@ -1,7 +1,9 @@
-import { Pool } from 'pg';
+import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+const { Pool } = pg;
 
 const connection = new Pool({
   connectionString: process.env.DATABASE_URL,

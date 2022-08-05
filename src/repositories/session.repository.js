4252ffc439f8355ecff675576d78a3ac83178877
Thales Alves <thalesalves758@gmail.com>
2022-08-05 16:19:00 -0300

    fix: fix column name

--- src/repositories/session.repository.js
+++ src/repositories/session.repository.js
@@ -3,7 +3,7 @@ import connection from '../config/database/pg.js';
 async function create(uuid, userId) {
   await connection.query(
     `
-    INSERT INTO sessions (session, "userId")
+    INSERT INTO sessions (session, user_id)
     VALUES ($1, $2)
   `,
     [uuid, userId]

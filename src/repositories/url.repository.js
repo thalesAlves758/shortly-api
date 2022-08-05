1151cef5b0c1354a522275692d3c148a59dca7bc
Thales Alves <thalesalves758@gmail.com>
2022-08-05 19:55:59 -0300

    feat: implement get shortened url feature

--- src/repositories/url.repository.js
+++ src/repositories/url.repository.js
@@ -10,4 +10,19 @@ async function create(url, shortenedUrl, userId) {
   );
 }
 
-export default { create };
+async function findById(id) {
+  const {
+    rows: [shortenedUrl],
+  } = await connection.query(
+    `
+    SELECT id, short_url AS "shortUrl", url, visit_count AS "visitCount", user_id AS "userId", created_at AS "createdAt"
+    FROM shortened_urls
+    WHERE id = $1
+  `,
+    [id]
+  );
+
+  return shortenedUrl;
+}
+
+export default { create, findById };
